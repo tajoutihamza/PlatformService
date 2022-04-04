831d6e68197d33261d93de65d722bacf15664615
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-04 12:59:18 +0100

    id increment automatically

--- PlatformService/Dtos/PlatformCreateDto.cs
+++ PlatformService/Dtos/PlatformCreateDto.cs
@@ -4,7 +4,6 @@ namespace PlatformService.Dtos
 {
     public class PlatformCreateDto
     {
-        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
