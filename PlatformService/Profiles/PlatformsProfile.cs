9a45b14dc9fe34d3eee3c69fe05034179207ead0
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-04 10:24:53 +0100

    data transefer dtos creation to create abstraction between data consuming and writing and exsiting data

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    {
+        
+        public PlatformsProfile()
+        {
+            //source --> target
+            CreateMap<Platform,PlatformReadDto>();
+            //target --> source
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+
+
+    }
+}
